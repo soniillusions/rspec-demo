fda7a1baa1204ae139152a59ff549c90723e0792
Aleksey <alekseycrosso@gmail.com>
2024-03-16 23:07:45 +0300

    add tests for power up and down

--- hero_spec.rb
+++ hero_spec.rb
@@ -7,5 +7,17 @@ describe Hero do
 
         expect(hero.name).to eq 'Mike'
     end
+
+    it "can power up" do
+        hero = Hero.new 'mike'
+
+        expect(hero.power_up).to eq 110
+    end
+
+    it "can power down" do
+        hero = Hero.new 'mike'
+
+        expect(hero.power_down).to eq 90
+    end
 end
 
